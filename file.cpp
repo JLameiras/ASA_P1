d8fad33abea8e75a4e7332a438c9997e6b41d19c
Lameiras <87664900+PLameiras@users.noreply.github.com>
2022-01-08 14:37:19 +0000

    ....

--- file.cpp
+++ file.cpp
@@ -3,15 +3,36 @@
 #include <string>
 using namespace std;
 
+void p1(){
+    string line;
+    getline(cin, line);
+    int ** sequence;
+    sequence = stringtToArray(line);
+
+}
+
+void p2(){
+
+
+}
+
+int ** stringToArray(string line){
+    int ** sequence;
+    for(int i = 0, int h = 0; line[i] != '\0'; i++) {
+        if (str[i] == ' ') j++;
+        else sequence[h] = 10 * sequence[h] + atoi(str[i]);
+    }
+}
+
 int main() {
     string line;
-    int programNumber;
+    int program;
 
     while(cin) {
         getline(cin, line);
-        programNumber = atoi(line);
-        if(programNumber == 1) p1();
-        else p2();
+        program = atoi(line);
+        if(program == 1) p1();
+        if(program == 2) p2();
     };
     return 0;
 }
