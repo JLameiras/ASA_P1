57bb37732e083d8a4184d3b28636d88205b5c110
Lameiras <87664900+PLameiras@users.noreply.github.com>
2022-01-09 17:27:21 +0000

    p2

--- file.cpp
+++ file.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector>
 
 using namespace std;
@@ -18,41 +17,47 @@ vector<int> stringToVector(string line){
 void p1(){
     string line;
     getline(cin, line);
+
     vector<int> sequence = stringToVector(line);
+    int sequenceSize = static_cast<int>(sequence.size());
     vector<int> lengthByID (sequence.size(), 1);
     vector<int> countByID (sequence.size(), 1);
     int length = 0;
     int count = 0;
 
-    for(int i = 0; i < sequence.size(); i++){
+    if(sequenceSize == 0 || sequenceSize == 1){
+        cout << sequenceSize << ' ' << sequenceSize;
+        return;
+    }
+
+    for(int i = 0; i < sequenceSize; i++){
         for(int j = 0; j < i; j++){
             if(sequence[j] >= sequence[i]) continue;
             if(lengthByID[i] < lengthByID[j] + 1){
                 lengthByID[i] = lengthByID[j] + 1;
                 countByID[i] = countByID[j];
             } else if(lengthByID[i] == lengthByID[j] + 1)
-                countByID[i] = countByID[i] + countByID[j];
+                countByID[i] += countByID[j];
         }
     }
 
-    for(int i : lengthByID) length = max(length, lengthByID[i]);
+    for(int i : lengthByID) length = max(length, i);
 
-    for(int i = 0; i < sequence.size(); i++)
-        if (lengthByID[i] == length)
-            count += countByID[i];
+    for(int i = 0; i < sequenceSize; i++)
+        if (lengthByID[i] == length) count += countByID[i];
 
-    cout << length << ' ' << count;
+    cout << length << ' ' << count << endl;
 }
 
 void p2() {
     string line;
     getline(cin, line);
     vector<int> sequence1 = stringToVector(line);
-    int n = sequence1.size();
+    int n = static_cast<int>(sequence1.size());
 
     getline(cin, line);
     vector<int> sequence2 = stringToVector(line);
-    int m = sequence2.size();
+    int m = static_cast<int>(sequence2.size());
 
     int A [n+1][m+1];
 
@@ -66,7 +71,7 @@ void p2() {
             else A[k][l] = A[k][l - 1];
         }
     }
-    cout << A[n][m];
+    cout << A[n][m] << endl;
 }
 
 int main() {
@@ -77,6 +82,6 @@ int main() {
         program = atoi(line.c_str());
         if(program == 1) p1();
         if(program == 2) p2();
-    };
+    }
     return 0;
 }
